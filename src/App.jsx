29e9ad0ab7ae0c9963a693dff104c6d2ec86d1f5
Luiz Cabral <luizfelipe.cabralsouza@gmail.com>
2023-01-31 20:18:54 -0300

    :sparkles: feat: configuração básica de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Home from '../src/pages/Home/Home';
+import Profile from '../src/pages/Profile/Profile';
 
 function App() {
 	return (
 		<>
-			<h1>Hello world!</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+					<Route path="/profile" exact element={<Profile />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
