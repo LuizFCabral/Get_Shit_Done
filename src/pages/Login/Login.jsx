f852f023a720f48ff3fcced101539b348846d1cd
natansemh <214natan@gmail.com>
2023-02-03 19:11:53 -0300

    :sparkles: feat: adicionando a estrutura da pagina Login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'React';
+import './Login.css';
+import Form from '../../components/Form/Form';
+
+const Login = () => {
+	return (
+		<div className="fundo">
+			<div className="logotipo">Logo</div>
+			<div className="containerForm">
+				<Form />
+			</div>
+		</div>
+	);
+};
+
+export default Login;
