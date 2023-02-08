bbc3ecdc0750794de5ab764e2c22e5cc386b81d7
natansemh <214natan@gmail.com>
2023-02-08 20:23:31 -0300

    :sparkles: feat: Componentização do Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'React';
+import './Button.css';
+
+const Button = ({ children, type, handleOnclick }) => {
+	return (
+		<button className="btn" type={type} onClick={handleOnclick}>
+			{children.toUpperCase()}
+		</button>
+	);
+};
+
+export default Button;
