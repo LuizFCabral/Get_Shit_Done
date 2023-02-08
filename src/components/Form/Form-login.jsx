5d8b93c7f08835e685c119c0bc0e171403a2ae6c
natansemh <214natan@gmail.com>
2023-02-08 20:25:45 -0300

    :sparkles: feat: Construindo o Form Login com compoenentes independentes

---
+++ src/components/Form/Form-login.jsx
@@ -0,0 +1,38 @@
+import React from 'React';
+import './Form.css';
+
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+
+const Formlogin = ({ titulo }) => {
+	return (
+		<form method="POST" className="form">
+			<div className="title-form">{titulo.toUpperCase()}</div>
+			<div className="inputContainer">
+				<div className="input-box">
+					<label>
+						<p>Email</p>
+						<Input
+							type="text"
+							name="email"
+							placeholder="Informe o seu e-mail"
+						/>
+					</label>
+				</div>
+				<div className="input-box">
+					<label>
+						<p>Senha</p>
+						<Input
+							type="password"
+							name="senha"
+							placeholder="Digite sua senha"
+						/>
+					</label>
+				</div>
+				<Button type="submit">login</Button>
+			</div>
+		</form>
+	);
+};
+
+export default Formlogin;
